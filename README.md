2c35cc6d67e9c9e4da1514ed522363444ce1b89d
Maxence Brette <maxbrette@gmail.com>
2022-09-13 13:32:30 -0500

    Application improvements, fixed prediction calculations, commenting and documentation.

--- README.md
+++ README.md
@@ -1,32 +1,32 @@
 # README
 
-## training
+## Prediction Model Training
 
 ```
 jupyternotebook
 ```
 
-## building
+## Building Docker Containers
 
-### sklearn
+### Base Python Scikit-Learn Docker Image
 ```
 docker build -t xnonr/sklearn:0.1.0 -f ./build/DockerFile.sklearn .
 ```
 
-### predicting docker image
+### Prediction Application Docker Image
 ```
 docker build -t xnonr/predictionapp:0.1.0 -f ./build/DockerFile.app .
 ```
 
-## predicting
+## Predicting
 
-### run the server manually
+### Manually Runs The Server
 
 ```
 uvicorn predicting.main:app --port 1313
 ```
 
-### run the server automatically in docker
+### Automatically Runs The Server Within A Docker Container
 
 ```
 docker run -d -p 1313:1313 --name prediction-api xnonr/predictionapp:0.1.0
--- predicting/main.py
+++ predicting/main.py
@@ -1,7 +1,8 @@
-# Imports required libraries
+# Imports Required Libraries
 import json
 import numpy as np
 import pandas as pd
+import pickle
 import statsmodels.api as sm
 
 from fastapi import FastAPI
@@ -10,8 +11,15 @@ from fastapi import Request
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import StandardScaler
 
-mdl = None
+# Global Variables
 
+# Properly filled in upon the application's startup
+mdl = None
+si = None
+ss = None
+
+# List of the already properly ordered column variables required by the pre-trained
+#    model in order for it to carry out accurate predictions
 final_df_column_variable_names_order = [
     'x5_saturday', 'x81_July', 'x81_December', 'x31_japan', 'x81_October',
     'x5_sunday', 'x31_asia', 'x81_February', 'x91', 'x81_May',
@@ -26,9 +34,17 @@ app = FastAPI(title='GLM', description='GLM API', version='1.0')
 
 
 @app.on_event('startup')
-def load_model():
+def load_models():
+    '''
+    Loads in pre-trained Prediction, SimpleImputer and StandardScalar models
+    '''
     global mdl
+    global si
+    global ss
+
     mdl = sm.load('models/glm_final_model.pickle')
+    si = pickle.load(open('../models/glm_simple_imputer.pickle', 'rb'))
+    ss = pickle.load(open('../models/glm_standard_scalar.pickle', 'rb'))
 
 
 @app.post('/predict')
@@ -37,15 +53,26 @@ async def get_prediction(info: Request):
     req_info = await info.json()
 
     df = extract_transform_input_data_pipeline(
-        req_info, final_df_column_variable_names_order)
+        req_info,
+        si,
+        ss,
+        final_df_column_variable_names_order)
 
     json_output_message = predict_outcome(
-        df, mdl, alphanumerically_sorted_df_column_variable_names)
+        df,
+        mdl,
+        alphanumerically_sorted_df_column_variable_names)
 
     return json_output_message
 
 
 def transform_json_to_df(json):
+    '''
+    Returns a DataFrame containing raw JSON data
+
+    Keyword Arguments:
+    json -- Raw JSON data
+    '''
 
     df = pd.read_json(json, orient='records')
 
@@ -53,8 +80,21 @@ def transform_json_to_df(json):
 
 
 def format_df_column_variables(df):
+    '''
+    Returns a DataFrame after having transformed those columns, whose values 
+        consisted of those of data types String and represented quantitative 
+        data, into values being that of the Float data type after removing any
+        non mathematical symbols 
 
-    # Formats the 'x12' column's String monetary value into a Float in order to apply maths upon it
+    Keyword Arguments:
+    df -- A DataFrame containing raw JSON data
+    '''
+
+    # Formats the 'x12' and 'x63' columns', consisting of the data type String,
+    #    and respectively representing first monetary then percentage values,
+    #    into the data type Float so as to be able to latter on apply mathermatical
+    #    work upon said columns' values later on
+
     df['x12'] = df['x12'].str.replace('$', '')
     df['x12'] = df['x12'].str.replace(',', '')
     df['x12'] = df['x12'].str.replace(')', '')
@@ -67,34 +107,40 @@ def format_df_column_variables(df):
     return df
 
 
-def impute_missing_df_data(df):
+def impute_missing_df_data(si, df):
+    '''
+    Returns a DataFrame with no column variables whose data is of a qualitative nature, 
+        as well as having filled in any remaining blank, NaN or NULL or otherwise missing 
+        values via the usage of an imported, pre-trained SimpleImputer using a mean based 
+        strategy
 
-    if df.shape[0] <= 1:
-        df = pd.DataFrame(df.drop(columns=['x5', 'x31', 'x81', 'x82']),
-                          columns=df.drop(columns=['x5', 'x31', 'x81', 'x82']).columns)
-        df = df.fillna(0)
+    Keyword Arguments:
+    si -- A pre-trained, imported SimpleImputer
+    df -- A formatted DataFrame
+    '''
 
-    else:
-        # Creates and instantiates a simple imputer
-        si = SimpleImputer(missing_values=np.nan, strategy='mean')
-
-        # Imputes via a simple mean stategy those column values which are missing
-        df = pd.DataFrame(si.fit_transform(df.drop(columns=['x5', 'x31', 'x81', 'x82'])),
-                          columns=df.drop(columns=['x5', 'x31', 'x81', 'x82']).columns)
+    df = pd.DataFrame(si.transform(df.drop(columns=['x5', 'x31', 'x81', 'x82'])),
+                      columns=df.drop(columns=['x5', 'x31', 'x81', 'x82']).columns)
 
     return df
 
 
-def scale_df_data(df):
-
-    # Creates and instantiates a standard scaler
-    ss = StandardScaler()
-
+def scale_df_data(ss, df):
     '''
-    Scales all column values via a standardization method for feature scaling, 
-        of particular interest and focus being being that of the monetary value column
+    Returns a DataFrame whose column variable values have all been scaled via a 
+        standardization method for the purpose of feature scaling, utilizing an
+        imported. pre-trained StandardScalar
+
+    Keyword Arguments:
+    ss -- A pre-trained, imported StandardScalar
+    df -- A formatted DataFrame without any blank, NaN, NULL or otherwise missing values
     '''
-    df = pd.DataFrame(ss.fit_transform(df),
+
+    # Of particular interest and focus is that of the 'x12' column representing
+    #    monetary values which tend to outscale all other column variable values
+    #    by some orders of magnitude
+
+    df = pd.DataFrame(ss.transform(df),
                       columns=df.columns)
 
     return df
@@ -102,9 +148,18 @@ def scale_df_data(df):
 
 def create_df_dummy_column_variables_new(df1, df2):
     '''
-    Creates the dummy variables for the non-numeric, qualitative data type columns and then reconcatenates
-        them back into the now imputed and standardized scaled dataframe
+    Returns a DataFrame with dummy variables for those column variables consisting of
+        qualitative data, as well as whose column variables of numeric quantitative data
+        have no missing values and are scaled
+
+    Keyword Arguments:
+    df1 -- A DataFrame containing the original raw JSON data in order to retrieve those column
+           variable values of a qualitative nature previously dropped and must now be 
+           dummified
+    df2 -- A DataFrame with no missing column variable values and whose said values have already been scaled
     '''
+
+    # A list of column variable names of a quantitative nature which require dummification
     vars_to_dummify = ['x5', 'x31', 'x81', 'x82']
 
     for var in vars_to_dummify:
@@ -123,6 +178,17 @@ def create_df_dummy_column_variables_new(df1, df2):
 
 
 def create_df_dummy_column_variables_old(df1, df2):
+    '''
+    Returns a DataFrame with dummy variables for those column variables consisting of
+        qualitative data, as well as whose column variables of numeric quantitative data
+        have no missing values and are scaled
+
+    Keyword Arguments:
+    df1 -- A DataFrame containing the original raw JSON data in order to retrieve those column
+           variable values of a qualitative nature previously dropped and must now be 
+           dummified
+    df2 -- A DataFrame with no missing column variable values and whose said values have already been scaled
+    '''
 
     x5_dummy_variables = pd.get_dummies(df1['x5'],
                                         drop_first=True,
@@ -159,21 +225,23 @@ def create_df_dummy_column_variables_old(df1, df2):
     return df2
 
 
-'''
-Filters out and retrives only those columns previously determined to be the most useful during the
-    creation of the prediction model
-'''
+def filter_df_column_variables(ordr_clmn_names_lst, df):
+    '''
+    Returns a DataFrame containing only those column variables required by the pre-trained model
+        for predictions, filtering out drom the given DataFrame only said columns
 
-
-def filter_df_column_variables(df, ordr_clmn_names_lst):
+    Keyword Arguments:
+    ordr_clmn_names_lst -- A list of DataFrame column variable names required by the pre-trained model
+    df -- The DataFrame with dummy variables, and whose quantitative values have been scaled and 
+          have none missing
+    '''
 
     necessary_clmn_vars_set = set(ordr_clmn_names_lst)
     avlbl_clmn_vars_set = set(df.columns)
 
-    '''
-    Depending upon the type and amount of data passed in, not all dummy variables will always be
-        successfully generated, necessitating their inclusion afterwards
-    '''
+    # Depending upon the type and amount of JSON data originally having been passed in,
+    #    not all of the desired dummy variables will always be successfully generated,
+    #    necessitating their inclusion afterwards via the code below
     if necessary_clmn_vars_set.issubset(avlbl_clmn_vars_set) == False:
         nan_df = pd.DataFrame(np.nan, index=range(
             df.shape[0]), columns=ordr_clmn_names_lst)
@@ -185,9 +253,20 @@ def filter_df_column_variables(df, ordr_clmn_names_lst):
     return df
 
 
-def extract_transform_input_data_pipeline(json_data, ordr_clmn_names_lst):
+def extract_transform_input_data_pipeline(json, si, ss, ordr_clmn_names_lst):
+    '''
+    Returns a DataFrame created from the passed in, raw JSON data, transforming said data via
+        imputation, to fill in any and all missing values, scaling, and the creation of dummy
+        variables for those qualitative column variables which require such action
 
-    df = transform_json_to_df(json_data)
+    Keyword Arguments:
+    json -- Raw JSON data
+    si -- A pre-trained, imported SimpleImputer
+    ss -- A pre-trained, imported StandardScalar
+    ordr_clmn_names_lst -- A list of DataFrame column variable names required by the pre-trained model
+    '''
+
+    df = transform_json_to_df(json)
 
     if df.shape[0] < 1:
         df = pd.DataFrame()
@@ -195,41 +274,68 @@ def extract_transform_input_data_pipeline(json_data, ordr_clmn_names_lst):
     else:
         df = format_df_column_variables(df)
 
-        imputed_df = impute_missing_df_data(df)
+        imputed_df = impute_missing_df_data(si, df)
 
-        scaled_imputed_df = scale_df_data(imputed_df)
+        scaled_imputed_df = scale_df_data(ss, imputed_df)
 
         df = create_df_dummy_column_variables_old(df, scaled_imputed_df)
 
-        df = filter_df_column_variables(df, ordr_clmn_names_lst)
+        df = filter_df_column_variables(ordr_clmn_names_lst, df)
 
     return df
 
 
-def predict_outcome(df, model, alphanum_ord_clmn_var_names_lst):
+def predict_outcome(df, mdl, alphanum_ord_clmn_var_names_lst):
+    '''
+    Returns a JSON message containing either the model's predicted outcomes, 
+        marked as 'business_outcome', predicted probability, marked as 'p_hat',
+        and the inputs in the alphanumerical order of their variables' names or,
+        should the predicted probability be under that of 75%, a message indicating
+        as such; if no valid JSON data as far as the application can tell is passed in, 
+        then a JSON encoded error message will be returned instead
+
+    Keyword Arguments:
+    df -- A DataFrame containing only those 25 column variables required by the pre-trained model
+    mdl -- A pre-trained, imported prediction model
+    alphanum_ord_clmn_var_names_lst -- A list of DataFrame column variable names required by 
+                                       the pre-trained model order alphanumerically
+    '''
 
     num_rows_df = df.shape[0]
 
     if num_rows_df == 0:
-        return json.dumps({'message': 'Error'})
+
+        return json.dumps({'message': 'ERROR - No valid JSON data available for prediction.'})
 
     else:
 
+        output_msgs_lst = []
+        count_output_msgs = 0
+
         for row in range(num_rows_df):
             predicted_outcome = 0
-            model_inputs = {}
+            mdl_inputs = {}
 
-            for var in alphanum_ord_clmn_var_names_lst:
-                model_inputs[var] = df.iloc[row][var]
-
-            predicted_probability = model.predict(df.iloc[row])[0]
+            predicted_probability = mdl.predict(df.iloc[row])[0]
 
             if predicted_probability >= 0.75:
                 predicted_outcome = 1
 
-            model_predictions = {'business_outcome': str(predicted_outcome),
-                                 'p_hat': str(predicted_probability)}
+            for var in alphanum_ord_clmn_var_names_lst:
+                mdl_inputs[var] = df.iloc[row][var]
 
-            output = model_predictions | model_inputs
+            mdl_predictions = {'business_outcome': str(predicted_outcome),
+                               'p_hat': str(predicted_probability)}
 
-            return json.dumps(output)
+            prediction_msg = mdl_predictions | mdl_inputs
+
+            output_msgs_lst.append(prediction_msg)
+
+            # else:
+            #output_msgs_lst.append("{'message': 'Business outcome probability too low.'}")
+            # pass
+
+            #count_output_msgs += 1
+            # print(count_output_msgs)
+
+        return json.dumps(output_msgs_lst)
