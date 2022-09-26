39277fa02841a06650309dce7f02e8cdf2837a16
Maxence Brette <maxbrette@gmail.com>
2022-09-26 17:09:00 -0500

    Bump Application version to 0.6.0

--- run_api.sh
+++ run_api.sh
@@ -1,5 +1,5 @@
 # On M1 Mac
-#docker run -d -p 1313:1313 --name prediction-api xnonr/predictionapp:0.5.0
+#docker run -d -p 1313:1313 --name prediction-api xnonr/predictionapp:0.6.0
 
 # On PC or other Linux based machine
-docker run -d -p 1313:1313 --name prediction-api xnonr/predictionapp-amd64:0.5.0
+docker run -d -p 1313:1313 --name prediction-api xnonr/predictionapp-amd64:0.6.0
--- run_api_kind_ingress.sh
+++ run_api_kind_ingress.sh
@@ -19,7 +19,7 @@ sleep 60
 kubectl get all -A
 
 # Retrieves a Docker Image and loads it into the kind Docker Container, a Docker image within a Docker container
-kind load docker-image xnonr/predictionapp:0.5.0 --name predictingcluster
+kind load docker-image xnonr/predictionapp:0.6.0 --name predictingcluster
 
 # Enters the Docker container itself and opens up a shell command line within
 #docker exec -ti predictingcluster-control-plane bash
